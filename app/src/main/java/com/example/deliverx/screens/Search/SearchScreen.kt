457b5bb4e6076e2f68bda18a9ce40eb97c2b31d2
Priyanshu Jha <priyanshujha2581@gmail.com>
2025-02-12 14:37:48 +0530

    Update SearchScreen.kt

--- app/src/main/java/com/example/deliverx/screens/Search/SearchScreen.kt
+++ app/src/main/java/com/example/deliverx/screens/Search/SearchScreen.kt
@@ -1,6 +1,7 @@
 package com.example.deliverx.screens.Search
 
 import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -14,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.graphics.Color
@@ -38,7 +40,10 @@ fun SearchScreen(navController: NavController) {
     ) {
         Column(modifier = Modifier
             .fillMaxSize()
-            .padding(10.dp))
+            .padding(10.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        )
         {
             val numberOptions = (1..5).toList()
 
