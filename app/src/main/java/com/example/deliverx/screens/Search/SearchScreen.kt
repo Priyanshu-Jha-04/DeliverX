a43f8251383a2d6d6add0452160c99eec0b3bcff
Priyanshu Jha <priyanshujha2581@gmail.com>
2025-03-02 22:56:39 +0530

    Update SearchScreen.kt

--- app/src/main/java/com/example/deliverx/screens/Search/SearchScreen.kt
+++ app/src/main/java/com/example/deliverx/screens/Search/SearchScreen.kt
@@ -1,9 +1,27 @@
 package com.example.deliverx.screens.Search
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.wear.compose.material.Text
 
 @Composable
 fun SearchScreen(navController: NavController) {
-
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column(modifier = Modifier.fillMaxSize().padding(10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center) {
+            Text("Search Screen", color = MaterialTheme.colorScheme.onPrimary)
+        }
+    }
 }
+
